66dd33d2552ea122889590043566af76cdcc96bc
oue-s <oue.soichi@tachibana-lab.jp>
2024-12-02 12:59:41 +0900

    a-6,8修正 bまで完了

--- a-6.py
+++ a-6.py
@@ -1,5 +1,5 @@
 odd_numbers = [1, 3, 5, 7, 9]
 
 # ここにコードを記述
-for i in range(5):
-    print(odd_numbers[i])
+for i in odd_numbers:
+    print(i)
--- a-8.py
+++ a-8.py
@@ -1,5 +1,5 @@
 users_info = [["Bob", 79], ["Tom", 59], ["Ken", 61]]
 
 # ここにコードを記述
-for i in range(3):
-    print(f"Name: {users_info[i][0]}, Age: {users_info[i][1]}")
+for i in users_info:
+    print(f"Name: {i[0]}, Age: {i[1]}")
