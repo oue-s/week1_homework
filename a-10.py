295baaf22e0d98d2b5820ca9928f682fd6e3dccd
oue-s <oue.soichi@tachibana-lab.jp>
2024-12-02 10:54:00 +0900

    add a-8-10

---
+++ a-10.py
@@ -0,0 +1,9 @@
+# ここにコードを記述
+import random
+
+
+def dice():
+    return random.randint(1, 6)
+
+
+print(dice())  # 1から6の整数をランダムに出力する
---
+++ a-8.py
@@ -0,0 +1,5 @@
+users_info = [["Bob", 79], ["Tom", 59], ["Ken", 61]]
+
+# ここにコードを記述
+for i in range(3):
+    print(f"Name: {users_info[i][0]}, Age: {users_info[i][1]}")
