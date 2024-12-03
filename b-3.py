466370104441e31628c1ac72cf6e032081aa69e1
oue-s <oue.soichi@tachibana-lab.jp>
2024-12-03 10:51:10 +0900

    modify b-3

--- b-3.py
+++ b-3.py
@@ -3,5 +3,8 @@ columns = int(input("列数を入力してください:"))
 
 for i in range(1, line + 1):
     for j in range(1, columns + 1):
-        print(f"{j} x {i} = {i * j} |", end=" ")
+        if i*j > 9:
+            print(f"{j} x {i} = {i * j} |", end=" ")
+        else:
+            print(f"{j} x {i} =  {i * j} |", end=" ")
     print("")
